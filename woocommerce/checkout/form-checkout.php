7266b245be6e33956afc32a29984c120359d2ae9
jimish soni <jimishsoni1990@outlook.com>
2023-11-17 18:45:43 +0530

    Code Optimized
    Moved some code blocks to main file with the help of do_action

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -34,29 +34,13 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 			<div class="owc-row">
 				<div class="owc-col left-container">
 
-					<?php
-						/*
-						*	Hooked:
-						*	owc_header_content - 10
-						*	owc_checkout_steps - 20
-						*
-						*/
-						do_action( 'owc_header', $checkout );
-					?>	
+					<?php do_action( 'owc_header', $checkout ); ?>	
 
 					<?php if( show_loggged_user_checkout_step() ): ?>
 
 						<div class="step-0 step-content logged-in-user">
 
-							<?php
-								/*
-								*	
-								*	
-								*	
-								*
-								*/
-								do_action( 'owc_logged_in_user_screen', $checkout );
-							?>
+							<?php do_action( 'owc_logged_in_user_screen', $checkout ); ?>
 
 						</div>
 
@@ -64,116 +48,46 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 					<div class="step-1 step-content">
 
+						<?php do_action( 'owc_step_one_top', $checkout ); ?>
+
 						<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-						<?php
-							/*
-							*	Hooked:
-							*	owc_information_content - 10
-							*	
-							*
-							*/
-							do_action( 'owc_information_box', $checkout );
-						?>
+						<?php do_action( 'owc_information_box', $checkout ); ?>
 
 						<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 
-						<div class="bottom-navigation">
-							<a class="go-back-btn owc-back-to-shop" href="<?php echo wc_get_page_permalink( 'shop' ); ?>">< Return to shop</a>
-							<button class="owc-shipping" type="button">Continue to shipping</button>
-						</div>
+						<?php do_action( 'owc_step_one_bottom', $checkout ); ?>
+
 					</div>
+
 					<div class="step-2 step-content">
-						<div class="information-review-box">
-							<table>
-								<tr class="contact">
-									<td>Contact</td>
-									<td width="100%" class="contact-email"></td>
-									<td><a class="owc-information" href="#owc-information">Change</a></td>
-								</tr>
-								<tr class="ship-to">
-									<td>Ship to</td>
-									<td class="ship-to-address"></td>
-									<td><a class="owc-information" href="#owc-information">Change</a></td>
-								</tr>
-							</table>
-						</div>
-						<?php
-							/*
-							*	Hooked:
-							*	owc_shipping_method_box - 10
-							*	
-							*
-							*/
-							do_action( 'owc_shipping_method', $checkout );
-						?>
-						<div class="bottom-navigation">
-							<a class="go-back-btn owc-information" href="#owc-information">< Return to information</a>
-							<button class="owc-payment">Continue to payment</button>
-						</div>
+
+						<?php do_action( 'owc_step_two_top', $checkout ); ?>
+
+						<?php do_action( 'owc_shipping_method', $checkout ); ?>
+						
+						<?php do_action( 'owc_step_two_bottom', $checkout ); ?>
+
 					</div>
+
 					<div class="step-3 step-content">
-						<div class="information-review-box">
-							<table>
-								<tr class="contact">
-									<td>Contact</td>
-									<td width="100%" class="contact-email"></td>
-									<td><a class="owc-information" href="#owc-information">Change</a></td>
-								</tr>
-								<tr class="ship-to">
-									<td>Ship to</td>
-									<td class="ship-to-address"></td>
-									<td><a class="owc-information" href="#owc-information">Change</a></td>
-								</tr>
-								<tr class="ship-method">
-									<td>Method</td>
-									<td class="shipping-method"></td>
-									<td><a class="owc-shipping" href="#owc-shipping">Change</a></td>
-								</tr>
-							</table>
-						</div>
-						<?php
-							/*
-							*	Hooked:
-							*	owc_billing_address_box - 10
-							*	owc_shipping_method_box - 20
-							*
-							*/
-							do_action( 'owc_step_3', $checkout );
-						?>
+
+						<?php do_action( 'owc_step_three_top', $checkout ); ?>
+
+						<?php do_action( 'owc_step_3', $checkout ); ?>
+
+						<?php do_action( 'owc_step_three_bottom', $checkout ); ?>
+
 					</div>
-					<?php
-						/*
-						*	
-						*	
-						*	
-						*
-						*/
-						do_action( 'owc_left_container_before_footer', $checkout );
-					?>
-					<div class="left-col-footer">
-						<?php
-							/*
-							*	
-							*	
-							*	
-							*
-							*/
-							do_action( 'owc_left_container_footer', $checkout );
-						?>
-					</div>
+					
+					<?php do_action( 'owc_left_container_footer', $checkout ); ?>
+
 				</div>
+
 				<div class="owc-col right-container">
-					<?php
-						/*
-						*	Hooked:
-						*	owc_order_summary - 10
-						*	
-						*
-						*/
-						do_action( 'owc_checkout_right_col_content', $checkout );
-					?>
+					<?php do_action( 'owc_checkout_right_col_content', $checkout ); ?>
 				</div>
+
 			</div>
 		</form>
 	</div>
